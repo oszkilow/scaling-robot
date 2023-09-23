fc68038dc95b6c0a37a290d90486f0dbfd4c6ae6
Oscar Agustin <124532075+oszkilow@users.noreply.github.com>
2023-09-23 15:39:42 -0600

    Add files via upload

---
+++ Hotel_Alura sql.sql1.sql
@@ -0,0 +1,10 @@
+SELECT 
+    *
+FROM
+    hotel_alura.usuarios;
+
+insert into usuarios (nombre, contraseña) values ('oscar','fuentes');
+insert into usuarios (nombre, contraseña) values ('hugo','agustin');
+insert into usuarios (nombre, contraseña) values ('diego','cien');
+insert into usuarios (nombre, contraseña) values ('monica','medina');
+insert into usuarios (nombre, contraseña) values ('raul','zalasar');
